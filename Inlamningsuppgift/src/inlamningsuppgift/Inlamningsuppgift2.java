7b5d05a905fb947d40a2d00bfc7aff9e51bf1e35
Johannes <mikae_hvp@hotmail.com>
2014-10-09 16:31:05 +0200

    Inlmaningsuppgift1
    Den är klar

---
+++ Inlamningsuppgift/src/inlamningsuppgift/Inlamningsuppgift2.java
@@ -0,0 +1,40 @@
+package inlamningsuppgift;
+
+import java.util.Scanner;
+
+    public class Inlamningsuppgift2 {
+    
+    public static void main(String[] args) {
+
+        Scanner user_input = new Scanner (System.in);
+        
+        String A;
+        System.out.println ("Vad heter du?");
+        A = user_input.next();
+        
+        String B;
+        System.out.println ("Vad heter du?");
+        B = user_input.next();
+        
+        String C;
+        System.out.println ("Vad heter du?");
+        C = user_input.next();
+        
+        String D;
+        System.out.println ("Vad heter du?");
+        D = user_input.next();
+        
+        String E;
+        System.out.println ("Vad heter du?");
+        E = user_input.next();
+        
+    
+    System.out.println(A + B + C + D + E );
+    
+    
+    
+    
+    
+    
+        }
+    }
---
+++ Inlamningsuppgift/src/inlamningsuppgift/inlamningsuppgift.java
@@ -0,0 +1,29 @@
+package inlamningsuppgift;
+
+import java.util.Scanner;
+ 
+
+public class inlamningsuppgift {
+
+   
+    public static void main(String[] args) {
+    
+        Scanner user_input = new Scanner (System.in);
+        
+        String name;
+        System.out.println ("Vad heter du?");
+        name = user_input.next();
+        
+        String Pnummer;
+        System.out.println ("Vad är ditt personummer?");
+        Pnummer = user_input.next();
+        Pnummer.substring(2, 5);
+        
+        String addres;
+        System.out.println ("Vad är din addres");
+        addres = user_input.next();
+        
+System.out.println("Välkommen " + name + " " + "då du fyller den " + Pnummer.substring(2, 6) + " " + "kommer vi komma till dig," + addres + " " + "och fira dig!");
+    }
+    
+}
