a1fc40212fe0bf4429fd22965c0cf49ac66d2764
Johannes <mikae_hvp@hotmail.com>
2014-10-13 15:09:27 +0200

    Revert "Revert "Nu ar allt klart!! :dancer:""
    This reverts commit e6d4ec68583da5a850846a14f3430da916e52b83.

--- Inlamningsuppgift/src/inlamningsuppgift/inlamningsuppgift.java
+++ Inlamningsuppgift/src/inlamningsuppgift/inlamningsuppgift.java
@@ -13,17 +13,25 @@ public class inlamningsuppgift {
         System.out.println ("Vad heter du?");
         name = user_input.next();
         
+        /* Här har jag bestämt att strängen blir det som användaren skriver in.
+           Det gjorde jag genom att använda java.util.Scanner, den låter 
+           användaren skriva in olika värden.*/
+           
+        
         String Pnummer;
         System.out.println ("Vad är ditt personummer?");
         Pnummer = user_input.next();
-        
-        // Här så har jag deffinerat en 
         
         String addres;
         System.out.println ("Vad är din addres");
         addres = user_input.next();
         
-System.out.println("Välkommen " + name + " " + "då du fyller den " + Pnummer.substring(2, 6) + " " + "kommer vi komma till dig," + addres + " " + "och fira dig!");
+        /* Nedanför så anväder jag substring till att ta ut vissa siffror i 
+           personnummret som användaren skriver in. */
+        
+        System.out.println
+        ("Välkommen " + name + " " + "då du fyller den " + Pnummer.substring(2, 6) 
+                + " " + "kommer vi komma till dig," + addres + " " + "och fira dig!");
     }
     
 }
