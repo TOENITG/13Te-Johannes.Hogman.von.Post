738779b81f83868a2610270d2edc6188910211bd
Johannes <mikae_hvp@hotmail.com>
2014-10-23 16:34:17 +0200

    Uppgift 3 med komentarer

--- Inlamningsuppgift/src/inlamningsuppgift/Inlämningsuppgift3.java
+++ Inlamningsuppgift/src/inlamningsuppgift/Inlämningsuppgift3.java
@@ -14,10 +14,17 @@ public class Inlämningsuppgift3 {
         double svar1 = tal1+tal2;
         System.out.println ("Addition:" + " " + svar1);
     
+        /* Här har jag använt Double.parseDouble()för att omvandla det användaren
+           skriver in till variablar som man kan addera, subtrahera, multiplicera
+           och dividera */
+        
         System.out.println ("Skriv ett tal som du vill subtrahera svaret med");
         double tal3 = Double.parseDouble(user_input.next());
         double svar2 = svar1-tal3;
         System.out.println ("Subtraktion:" + " " + svar2);
+        
+        /* Jag har tagit svaret från den tidigare uträkning och använt det i denna
+           uträkning */
         
         System.out.println ("Skriv ett tal som du vill multiplicera svaret med");
         double tal4 = Double.parseDouble(user_input.next());
